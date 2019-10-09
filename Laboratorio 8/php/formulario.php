f4bed101f60e46edcb2b812594eb9bbb32769129
Randy Choc <rchocm@miumg.edu.gt>
2019-10-09 04:07:00 -0600

    Lab 8 CV #4
    +Ya se puede almacenar en una BD

--- Laboratorio 8/php/formulario.php
+++ Laboratorio 8/php/formulario.php
@@ -1,40 +1,23 @@
 <?php
-    //conectarnos con el servidor 
-    //$conectar = mysql_connect('localhost', 'root', '');
-    //Creas una variable de tipo objeto mysqli con los datos de la bd y el charset que quieras
-    $conectar = new mysqli('127.0.0.1', 'root', '', 'web');
-    $conectar->set_charset("utf8");
-    //verificamo que la conexion sea correcta 
-    if (!$conectar) 
-    {
-        echo "No se pudo conectar con el servidor";        
+    $mysqli = new mysqli("localhost", "root", "", "web");
+    if ($mysqli->connect_errno) {
+        echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
     else
     {
-        //$base = mysql_select_db('web');
-        //if (!$base) 
-        //{
-            echo "No se encontro la BD Web";
-        //}
+        //recuperar las variables del formulario 
+        $nombreCompleto = $_POST['txtNombreApellido'];
+        $correo = $_POST['txtCorreo'];
+        $mensaje = $_POST['txtAyuda'];
+        //verificamos que se ejecute correctamente el query
+        if (!$mysqli->query("INSERT INTO datos (nombreCompleto, correo, mensaje) VALUES ('$nombreCompleto', '$correo', '$mensaje')")) 
+        {
+            echo "Falló la creación de la tabla: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        else
+        {
+            echo "Datos guardados correctamente! ";
+        }    
     }
-    //recuperar las variables del formulario 
-    $nombreCompleto = $_POST['txtNombreApellido'];
-    echo "mi variable es: " . $nombreCompleto;
-    $correo = $_POST['txtCorreo'];
-    $mensaje = $_POST['txtAyuda'];
-    //hacemos la sentencia de sql para guardar los datos 
-    $sql = "INSERT INTO datos VALUES ('$nombreCompleto', '$correo', '$mensaje')";
-    //ejecutamos la sentencia 
-    $ejecutar = mysql_query($sql);
-    //verificamos la ejecucion del query
-    if (!$ejecutar) 
-    {
-        echo "Hubo algun error!";
-    }
-    else
-    {
-        echo "Datos guardados correctamente! ";
-    }
-    //<br> <a href='PaginaWebLab8.html'> Volver </a>
 ?>
 <br> <a href='PaginaWebLab8.html'> Volver </a>
